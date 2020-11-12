d8bd85a9c9a91fb4df84ae683523c0812ab537ef
Themd <themd@mail.ru>
2020-11-12 14:05:48 +0200

    update 0.3

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const app = express()
 
 const PORT = process.env.PORT || 80
 
+app.get('/', (req, res) => {
+  res.end('<h1>Home page</h1>')
+})
+
 app.listen(PORT, () => {
   console.log('Server started')
 })
