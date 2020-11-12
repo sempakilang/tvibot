0ada61b5f36866e0cb573947097b27e4c84b336c
Themd <themd@mail.ru>
2020-11-12 14:54:01 +0200

    update 0.7

--- index.js
+++ index.js
@@ -12,11 +12,10 @@ app.get('/', (req, res) => {
   res.end('<h1>Home page</h1>')
 })
 
-// app.post('/5t8WO9qaGdUGQfCEfhDZ', (req, res) => {
-//     // const name = req.body.name
-//     console.log(req);
-// res.end('')
-// })
+app.post('/5t8WO9qaGdUGQfCEfhDZ', (req, res) => {
+  console.log(req.body);
+  res.end('')
+})
 
 app.get('/7BxCeBZrp8', (req, res) => {
   console.log("req:", req.body);
